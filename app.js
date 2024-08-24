1dace19f76a18b72ed3956bedfaa8005ceb4bf38
James <rotichjames8@gmail.com>
2024-08-24 12:05:07 +0300

    node practice

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
-const request = require("request");
+const yargs = require("yargs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const url =
-  "https://api.weatherstack.com/current?access_key=284f334f688bfdd999f9eb5902b2ffda&query=karatina";
+const address = process.argv[2];
 
-const geocodingURL =
-  "https://api.mapbox.com/search/geocode/v6/forward?q=karatina&worldview=cn&access_token=pk.eyJ1IjoiamFtZXNydGMwIiwiYSI6ImNtMDQ3MmVpMTA2cnUyanM0MmRvM2puYWUifQ.bhijM8QPLfFKic9GbolrYw&limit=1";
+if (!address) {
+  console.log("please provide an address");
+} else {
+  geocode(address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return console.log(error);
+    }
 
-request({ url, json: true }, (error, response) => {
-  if (error) {
-    console.log("unable to connect to weather service");
-  } else if (response.body.error) {
-    console.log("unable to find location");
-  } else {
-    const data = response.body.current;
-    console.log(
-      "it is currently " +
-        data.temperature +
-        " degrees but it feels like " +
-        data.feelslike +
-        " degrees out"
-    );
-  }
-});
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(location);
+      console.log(forecastData);
+    });
+  });
+}
 
-request({ url: geocodingURL, json: true }, (error, response) => {
-  if (error) {
-    console.log("unable to connect to location service");
-  } else if (response.body.features.length === 0) {
-    console.log("unable to find location");
-  } else {
-    const data = response.body.features[0].properties.coordinates;
-    const latitude = data.latitude;
-    const longitude = data.longitude;
-    console.log(latitude, longitude);
-  }
-});
+// yargs.command({
+//   command: "getWeather",
+//   describe: "get weather for location",
+//   builder: {
+//     location: {
+//       describe: "weather for location",
+//       demandOption: true,
+//       type: "string",
+//     },
+//   },
+//   handler(argv) {
+//     if (argv.location) {
+//       geocode(argv.location, (error, data) => {
+//         if (error) {
+//           return console.log(error);
+//         }
+
+//         forecast(data.latitude, data.longitude, (error, forecastData) => {
+//           if (error) {
+//             return console.log(error);
+//           }
+//           console.log(data.location);
+//           console.log(forecastData);
+//         });
+//       });
+//     }
+//   },
+// });
